5a2c4a42bc40ddb85c786f0ea36db97a096775f9
trong hieu <hieu745233@gmail.com>
2023-02-16 09:01:16 +0700

    Update

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -8,15 +8,24 @@ import Service from './Service';
 import CicleCate from './CircleCate';
 import Selling from './Selling';
 import Product from './Product';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import { faGamepad, faGraduationCap, faLaptop, faPenRuler } from '@fortawesome/free-solid-svg-icons';
+import { useEffect, useRef } from 'react';
 
 
 const cx = classNames.bind(styles)
 
 function Home() {
+    gsap.registerPlugin(ScrollTrigger);
     const CateIcon = CicleCate
+    const ref = useRef(null)
+    useEffect(()=>{
+        
+    },[])
+
     return (
-        <div className={cx('wrapper')}>
+        <div className={cx('wrapper-full')} ref={ref}>
             <div className={cx('banner')}>
                 <div className={cx('banner-card')}>
                     <div className={cx('container-img-left')}>
@@ -28,13 +37,13 @@ function Home() {
                     </div>
                 </div>
                 <div className={cx('service-container')}>
-                    <Service />
-                    <Service />
-                    <Service />
-                    <Service />
+                    <Service className={cx('ser')} />
+                    <Service className={cx('ser')} />
+                    <Service className={cx('ser')} />
+                    <Service className={cx('ser')} />
                 </div>
             </div>
-                <div className={cx('wrapper')}>
+            <div className={cx('wrapper')}>
                     <h1 style={{
                             fontSize:'30px',
                             fontWeight:'600',
@@ -50,22 +59,22 @@ function Home() {
                         <div><CateIcon>{faLaptop}</CateIcon><p>Laptop Gaming</p></div>
                         <div><CateIcon>{faGamepad}</CateIcon><p>Laptop Gaming</p></div>
                     </div>
-                </div>
+            </div>
             <div className={cx('wrapper')}>
                     <h1 style={{
                             fontSize:'30px',
                             fontWeight:'600',
                             color:'#1d1d1d',
                             lineHeight:'36px'
-                    }}>
+                    }} className={cx('test')}>
                     Selling Product
                     </h1>
-                <div className={cx('selling-container')}>
-                    <Selling/>
-                    <Selling/>
-                    <Selling/>
-                    <Selling/>
-                </div>
+            <div className={cx('selling-container')}>
+                <Selling/>
+                <Selling/>
+                <Selling/>
+                <Selling/>
+            </div>
                 <div className={cx('wrapper')}>
                     <h1 style={{
                             fontSize:'30px',
@@ -83,7 +92,7 @@ function Home() {
                     </div>
                 </div>
             </div>
-            </div>
+        </div>
     );
 }
 
