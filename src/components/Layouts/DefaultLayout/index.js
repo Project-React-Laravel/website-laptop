c342ff5c2bb466abb6ea81e9a148daf2b26b0bcf
QUANG TRƯỜNG <88566977+TRUONG17052001@users.noreply.github.com>
2023-03-01 10:26:40 +0700

    admin

--- src/components/Layouts/DefaultLayout/index.js
+++ src/components/Layouts/DefaultLayout/index.js
@@ -1,22 +1,28 @@
-import Header from "./Header";
-import Footer from "./Footer";
-import styles from './DefaultLayout.module.scss'
+import Header from './Header';
+import Footer from './Footer';
+import styles from './DefaultLayout.module.scss';
 import classNames from 'classnames/bind';
 
-const cx = classNames.bind(styles)
+const cx = classNames.bind(styles);
 
-
-
-function DefaultLayout({children}) {
-    return (
-        <div className={cx('wrapper')}>
-            <Header />
-            <div className={cx('container')}>
-                    {children}
+function DefaultLayout({ children,admin }) {
+    if(admin){
+        return (
+            <div className={cx('wrapper')}>
+                <Header />
+                {/* <div className={cx('container')}>{children}</div>
+                <Footer /> */}
             </div>
-            <Footer />
-        </div>
-    );
+        );
+    }else{
+        return (
+            <div className={cx('wrapper')}>
+                <Header />
+                <div className={cx('container')}>{children}</div>
+                <Footer />
+            </div>
+        );
+    }
 }
 
 export default DefaultLayout;
